737c20619697721196764f5155f7584ff799e11c
nileshplays <nileshsarkar7731@gmail.com>
2024-07-07 14:20:39 +0530

    changes

--- Binary Search Tree/searchInBST.cpp
+++ Binary Search Tree/searchInBST.cpp
@@ -2,6 +2,7 @@
     Following is the Binary Tree node structure:
 
     template <typename T>
+
     class BinaryTreeNode
     {
     public:
@@ -11,8 +12,8 @@
         BinaryTreeNode(T x) : data(x), left(NULL), right(NULL) {}
         BinaryTreeNode(T x, BinaryTreeNode<T> *left, BinaryTreeNode<T> *right) : data(x), left(left), right(right) {}
     };
-
 */
+
 
 bool searchInBST(BinaryTreeNode<int> *root, int x) {
     // Write your code here.
