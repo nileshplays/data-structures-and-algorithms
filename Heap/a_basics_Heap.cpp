a8fe1f083479b665ce204cecbf3f228b1cb07b68
nileshplays <nileshsarkar7731@gmail.com>
2024-07-26 18:51:44 +0530

    Kth Smallest Element HEAP

--- Heap/a_basics_Heap.cpp
+++ Heap/a_basics_Heap.cpp
@@ -53,6 +53,34 @@ class Heap{
 
     }
 
+    void heapify(int arr[], int n , int i){
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i +1;
+
+        if(left <= n && arr[largest] < arr[i]){
+            largest = left;
+        }
+        if(right <= n && arr[largest] < arr[i]){
+            largest = right;
+        }
+        
+        if(largest != i){
+            swap(arr[largest] , arr[i]);
+            heapify(arr , n ,largest);
+        }
+    }
+
+    void heapSort(int arr[] , int n ){
+        int size = n;
+        while(size > 1){
+            swap(arr[size] , arr[1]);
+            size--;
+            
+            heapify(arr , size ,1);
+        }
+    }
+
     void print(){
         for(int i=1; i<= size ;i++){
             cout<<arr[i] <<" ";
@@ -64,6 +92,7 @@ class Heap{
 int main(){
     // int value; cin>>value;
     Heap h;
+    /*
     h.insert(12);
     h.insert(50);
     h.print();
@@ -84,5 +113,45 @@ int main(){
     h.print();
     h.deleteFromHeap();
     h.print();
+    */
+/*
+   int arr[6] = {-1, 54,53, 55,52,50};
+   int n = 5;
+    for(int i = n/2 ; i>0 ; i--){
+        h.heapify(arr,  n , i);
+    }
+    cout<<"Printing array :"<<endl;
+    for(int i =1 ; i<= n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    h.heapSort(arr, n );
+    cout<<"Printing Sorted array :"<<endl;
+    for(int i =1 ; i<= n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+*/
+
+    priority_queue<int> pq;
+    pq.push(3);
+    pq.push(1);
+    pq.push(7);
+    pq.push(8);
+    pq.push(2);
+    pq.push(5);
+    cout<<"MaxHeap top: "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"MaxHeap top: "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"MaxHeap top: "<<pq.top()<<endl;
+
+    priority_queue<int, vector<int> , greater<int> > minHeap;
+
+    minHeap.push(7);
+    minHeap.push(3);
+    minHeap.push(6);
+    cout<<"MinHeap top: "<<minHeap.top()<<endl;
+    minHeap.pop();
+    cout<<"MinHeap top: "<<minHeap.top()<<endl;
     return 0;
 }
